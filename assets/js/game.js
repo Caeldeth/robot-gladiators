715c3649fa36080a496899a012909eefdb694659
Thomas Cole <tacolejr@gmail.com>
2021-08-24 18:05:24 -0500

    initial javascript setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,13 @@
-window.alert("This is an alert! Javascript is running!");
+var playerName = window.prompt("What is your robot's name?");
+
+// track the robot's name
+console.log("Our robot's name is " + playerName);
+
+window.alert(playerName);
+
+// this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begin!");
+}
+
+// fight();
